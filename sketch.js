914fe2a48d151639a802103a246f73c6f097e2bf
garyle1 <62418420+garyle1@users.noreply.github.com>
2021-05-04 16:02:22 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://garyle1.github.io/001/';
+let imageModelURL = 'https://garyle1.github.io/0011/';
 
 // Video
 let video;
